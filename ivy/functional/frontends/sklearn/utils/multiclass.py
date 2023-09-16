c4e5bf4e22ea22de700b75bbc30a401ed2968a9f
root <rajaditya3275@gmail.com>
2023-09-16 07:20:44 +0000

    added helper function _num_samples in utils.validation in sklearn

--- ivy/functional/frontends/sklearn/utils/multiclass.py
+++ ivy/functional/frontends/sklearn/utils/multiclass.py
@@ -1,9 +1,11 @@
 import ivy
-
+from ivy.functional.frontends.numpy.func_wrapper import inputs_to_ivy_arrays
 
 # reapeated utility function
+@inputs_to_ivy_arrays
 def type_of_target(y, input_name="y"):
     # purely utility function
+    y = ivy.array(y)
     unique_vals = len(ivy.unique_values(y))
     if y.ndim == 2 and y.shape[1] > 1 and unique_vals <= 2:
         return "multilabel-indicator"
